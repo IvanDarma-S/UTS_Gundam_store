5886ccb363fc4755974056e3d50267e33e00dff7
IvanDarma-S <ivandarma3000@gmail.com>
2026-04-12 12:18:08 +0700

    feat: penambahan file di dashboard > domain > repositories
    menambahkan file product_repository.dart dan product_repository_impl.dart

---
+++ lib/features/dashboard/domain/repositories/product_repository.dart
@@ -0,0 +1,10 @@
+import 'package:apps_marketplace_integration_backend/features/dashboard/data/models/product_model.dart';
+
+abstract class ProductRepository {
+  Future<List<ProductModel>> getProducts({
+    int page = 1,
+    int limit = 10,
+    String? category,
+  });
+  Future<ProductModel> getProductById(int id);
+}
